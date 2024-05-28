4101f02c8d23672c637dc0d2c750eb45fc9186e7
Влад Кантамиров <kantamirov2016@gmail.com>
2024-05-28 15:49:51 +0300

    create views directory and add routes.py

--- app.py
+++ app.py
@@ -1,37 +1,22 @@
 from bottle import Bottle, run, request, static_file, template, response
-import json
+import json, os
+import routes
 
 app = Bottle()
 
-@app.route('/')
-def index():
-    return template('index')
-
-# Страницы для перехода
-@app.route('/page1')
-def page1():
-    return '<h1>Page 1</h1><p>This is the content of page 1.</p>'
-
-@app.route('/page2')
-def page2():
-    return '<h1>Page 2</h1><p>This is the content of page 2.</p>'
-
-@app.route('/page3')
-def page3():
-    return '<h1>Page 3</h1><p>This is the content of page 3.</p>'
-
-@app.route('/page4')
-def page4():
-    return '<h1>Page 4</h1><p>This is the content of page 4.</p>'
-
-@app.route('/about')
-def authors():
-    return template('about')
-
-# Маршрут для статических файлов (CSS)
-@app.route('/static/<filepath:path>')
-def server_static(filepath):
-    return static_file(filepath, root='./static')
-
 if __name__ == '__main__':
     run(app, host='localhost', port=8080, debug=True)
+
+    PROJECT_ROOT = os.path.abspath(os.path.dirname(__file__))
+    STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static').replace('\\', '/')
+    HOST = os.environ.get('SERVER_HOST', 'localhost')
+    try:
+        PORT = int(os.environ.get('SERVER_PORT', '443'))
+    except ValueError:
+        PORT = 443
+
+    @Bottle.route('/static/<filepath:path>')
+    
+    
+    def server_static(filepath):
+        return Bottle.static_file(filepath, root=STATIC_ROOT)
