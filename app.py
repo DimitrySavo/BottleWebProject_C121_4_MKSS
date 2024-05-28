d6d290e9bb1acbd76dac4adcb3099e740170982e
DimitryS <dimitrysavo@gmail.com>
2024-05-28 16:01:41 +0300

    fix routes and app py files

--- app.py
+++ app.py
@@ -1,22 +1,10 @@
 from bottle import Bottle, run, request, static_file, template, response
-import json, os
-import routes
+from routes import *
 
 app = Bottle()
 
-if __name__ == '__main__':
-    run(app, host='localhost', port=8080, debug=True)
+app.route('/', 'GET', index)
+app.route('/about', 'GET', about)
 
-    PROJECT_ROOT = os.path.abspath(os.path.dirname(__file__))
-    STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static').replace('\\', '/')
-    HOST = os.environ.get('SERVER_HOST', 'localhost')
-    try:
-        PORT = int(os.environ.get('SERVER_PORT', '443'))
-    except ValueError:
-        PORT = 443
-
-    @Bottle.route('/static/<filepath:path>')
-    
-    
-    def server_static(filepath):
-        return Bottle.static_file(filepath, root=STATIC_ROOT)
+if __name__ == "__main__":
+    app.run()
--- routes.py
+++ routes.py
@@ -1,29 +1,24 @@
 from bottle import Bottle, run, request, static_file, template, response, route
 import json
 
-@route('/')
 def index():
     return template('index')
 
 # Страницы для перехода
-@route('/page1')
 def page1():
     return '<h1>Page 1</h1><p>This is the content of page 1.</p>'
 
-@route('/page2')
 def page2():
     return '<h1>Page 2</h1><p>This is the content of page 2.</p>'
 
-@route('/page3')
 def page3():
     return '<h1>Page 3</h1><p>This is the content of page 3.</p>'
 
-@route('/page4')
+
 def page4():
     return '<h1>Page 4</h1><p>This is the content of page 4.</p>'
 
-@route('/about')
-def authors():
+def about():
     return template('about')
 
 # Маршрут для статических файлов (CSS)
