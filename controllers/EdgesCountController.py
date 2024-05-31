8bef749f1e9b35fe85000e798add09382787208f
DimitryS <dimitrysavo@gmail.com>
2024-05-31 14:30:32 +0300

    add controller for degrees method on edgescount page

--- controllers/EdgesCountController.py
+++ controllers/EdgesCountController.py
@@ -42,3 +42,21 @@ def create_graph():
     response.content_type = 'application/json'
     #отправка ответа
     return json.dumps({'is_connected': is_connected, 'matrix': matrix, 'image_base64': image_base64})
+
+def degrees_for_graphs():
+    data = request.json
+    size1 = data["size1"]
+    edges1 = data["edges1"]
+    size2 = data["size2"]
+    edges2 = data["edges2"]
+
+    graph1 = MGraph(size1)
+    graph2 = MGraph(size2)
+
+    graph1.add_nodesAndEdges(size1, edges1)
+    graph2.add_nodesAndEdges(size2, edges2)
+
+    degrees1 = graph1.degrees()
+    degrees2 = graph2.degrees()
+
+    return json.dumps({'degrees1' : degrees1, 'degrees2' : degrees2, })
