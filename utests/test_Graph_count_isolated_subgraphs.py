b39622698d86cb4b5c7486fb3f9bf3e5d7efa037
Mov <mamedovemil366@gmail.com>
2024-06-06 10:06:01 +0300

    update proekt

--- utests/test_Graph_count_isolated_subgraphs.py
+++ utests/test_Graph_count_isolated_subgraphs.py
@@ -30,6 +30,23 @@ class TestCountIsolatedSubgraphs(unittest.TestCase):
         graph5 = Graph(6)
         graph5.add_edges_from([[0, 1], [2, 3], [4, 5]])
         self.assertEqual(graph5.count_isolated_subgraphs(), 3)
+        
+        # Тест 6: Граф с 6 узлами 
+        # Матрица смежности:
+        # [0, 0, 0, 0, 0, 0]
+        # [0, 0, 1, 1, 0, 0]
+        # [0, 1, 0, 1, 0, 0]
+        # [0, 1, 1, 0, 0, 0]
+        # [0, 0, 0, 0, 0, 1]
+        # [0, 0, 0, 0, 0, 1]
+        graph6 = Graph(6)
+        graph6.add_edges_from([
+            [1, 2], [1, 3], 
+            [2, 1], [2, 3], 
+            [3, 1], [3, 2], 
+            [4, 5], [5, 4]
+        ])
+        self.assertEqual(graph6.count_isolated_subgraphs(), 3)
 
 if __name__ == '__main__':
     unittest.main()
